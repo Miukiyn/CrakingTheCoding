4f278de5fca53800f3da9caaebc9a4f09c6ae5a6
Miukiyn <iagoadrien@gmail.com>
2025-05-26 09:34:11 -0300

    Update CheckPermutation

--- CheckPermutation/Permutation.java
+++ CheckPermutation/Permutation.java
@@ -1,1 +1,20 @@
-w
+public class Permutation{
+
+    /**
+     * 
+     * @param words
+     * @return 
+     */
+    public boolean isPermutation(String[] words){
+
+        boolean[] hash = new boolean[128]; // all values ASCII typed
+
+        for(int i = 0; i < words[0].length(); i++){
+            hash[words[0].charAt(i)] = true;
+        }
+
+        
+
+        return false;
+    }
+}
