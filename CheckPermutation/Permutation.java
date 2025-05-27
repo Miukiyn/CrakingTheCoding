afb505942a696e1130742749134b81503aeda21b
Miukiyn <71103276+Miukiyn@users.noreply.github.com>
2025-05-27 12:13:31 -0300

    Update Permutation.java

--- CheckPermutation/Permutation.java
+++ CheckPermutation/Permutation.java
@@ -1,4 +1,23 @@
+/**
+* Author: Iago Adrien - Miukiyn
+* Version: 1.0
+*/
+import java.io.BufferedReaderStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Permutation{
+
+    public static void main(String[] agrs){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            String in = reader.readLine();
+            
+        }catch(IOException e){
+            System.out.println("Erro: " + e);
+        }
+
+    }
 
     /**
      * 
@@ -12,6 +31,15 @@ public class Permutation{
         for(int i = 0; i < words[0].length(); i++){
             hash[words[0].charAt(i)] = true;
         }
+        
+        for(int i = 0; i < words[1].length(); i++){
+            if(!(hash[words[0].charAt(i) && hash[words[1].charAt(i))){
+                return false;
+            }
+            return true;
+        }
+
+        
 
         
 
