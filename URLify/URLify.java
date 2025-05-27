d03ffb050e00082ab6e4aab46664065dc89ebd67
Miukiyn <71103276+Miukiyn@users.noreply.github.com>
2025-05-27 15:24:52 -0300

    Create URLify.java

---
+++ URLify/URLify.java
@@ -0,0 +1,20 @@
+/**
+* Author: Iago Adrien - Miukiyn
+* Version: 1.0
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class URLify{
+  
+  /**
+  *
+  * @param 
+  */
+  public static void main(String[] agr){
+    
+  }
+
+  
+}
